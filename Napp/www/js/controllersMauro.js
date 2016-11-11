5446e7b4fe6168962ddccfe59c0fcd52810d7fea
tschm2 <emautsc2@gmail.com>
2016-11-11 14:19:01 +0100

    kontakte züg

--- Napp/www/js/controllersMauro.js
+++ Napp/www/js/controllersMauro.js
@@ -1,8 +1,24 @@
 angular.module('starter.controllersMauro', [])
 
-.controller('KontakteCtrl', function($scope, $location) {
-    5
+.controller('KontakteCtrl', function($scope, $ionicPopup, $location) {
+
+  $scope.showPrompt = function() {
+ var promptPopup = $ionicPopup.prompt({
+    title: translate="APPOINTMENTS",
+    template: 'This is prompt popup'
+ });
+ promptPopup.then(function(res) {
+    if (res) {
+       console.log('Your input is ', res);
+    } else {
+       console.log('Please enter input');
+    }
+ });
+
+};
   })
   .controller('InfosCtrl', function($scope, $location) {
+
+
 
   })
