1d7a4d0f775ab1dad9282f911086f66edf3e3612
tschm2 <emautsc2@gmail.com>
2016-11-12 14:35:49 +0100

    Revert "Revert "Add & Remove Contacts DONE :+1:""
    This reverts commit 192b462d6ec8cf38fe94cd344591833386f3433d.

--- Napp/www/js/controllersMauro.js
+++ Napp/www/js/controllersMauro.js
@@ -1,19 +1,36 @@
 angular.module('starter.controllersMauro', [])
 
 .controller('KontakteCtrl', ['$scope', function($scope, $location) {
+
 $scope.myContacts = [];
+
+$scope.myContacts.push({
+func:"Hausarzt",
+name:"Stefan Knöpfli",
+street:"Bielstrasse 25",
+location:"2051 Biel",
+phone:"+4131312321223",
+mail:"stefan.knöpfli@szb.ch"});
+
+$scope.myContacts.push({
+func:"Hausarzt",
+name:"Meier Max",
+street:"Bielstrasse 25",
+location:"2051 Biel",
+phone:"+4131312321223",
+mail:"stefan.knöpfli@szb.ch"});
+
+$scope.myContacts.push({
+func:"Hausarzt",
+name:"Hans Peter",
+street:"Bielstrasse 25",
+location:"2051 Biel",
+phone:"+4131312321223",
+mail:"stefan.knöpfli@szb.ch"});
+
 $scope.addContact = {};
 $scope.addContact = function(){
 
-  /*  var contact = [{
-      func:$scope.addContact.func,
-      name:$scope.addContact.nm,
-      street:$scope.addContact.street,
-      location:$scope.addContact.loc,
-      phone:$scope.addContact.phone,
-      mail:$scope.addContact.mail}];
-
-    $scope.myContacts.push(contact);*/
     $scope.myContacts.push({
     func:$scope.addContact.func,
     name:$scope.addContact.nm,
@@ -21,6 +38,19 @@ $scope.addContact = function(){
     location:$scope.addContact.loc,
     phone:$scope.addContact.phone,
     mail:$scope.addContact.mail});
+    /*  var contact = [{
+        func:$scope.addContact.func,
+        name:$scope.addContact.nm,
+        street:$scope.addContact.street,
+        location:$scope.addContact.loc,
+        phone:$scope.addContact.phone,
+        mail:$scope.addContact.mail}];
+
+      $scope.myContacts.push(contact);*/
+}
+
+$scope.removeContact = function(event){
+angular.element( document.querySelector('#contact'+event.target.id)).addClass('hidden');
 }
 
 
@@ -31,6 +61,7 @@ form.removeClass('hidden');
 angular.element( document.querySelector( '#minus' )).removeClass('hidden');
 angular.element( document.querySelector( '#plus' )).removeClass('hidden');
 angular.element( document.querySelector( '#contacts' )).addClass('hidden');
+angular.element( document.getElementsByClassName( 'contacts' )).addClass('hidden');
 
 
 };
@@ -41,11 +72,10 @@ form.addClass('hidden');
 angular.element( document.querySelector( '#minus' )).addClass('hidden');
 angular.element( document.querySelector( '#plus' )).addClass('hidden');
 angular.element( document.querySelector( '#contacts' )).removeClass('hidden');
-
+angular.element( document.getElementsByClassName( 'contacts' )).removeClass('hidden');
 };
   }])
   .controller('InfosCtrl', function($scope, $location) {
-
 
 
   })
