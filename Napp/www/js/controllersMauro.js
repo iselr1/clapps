dd036a39cd81ada99f960a016ad3cd10a46a915a
tschm2 <emautsc2@gmail.com>
2016-11-12 10:48:05 +0100

    contacts
    da

--- Napp/www/js/controllersMauro.js
+++ Napp/www/js/controllersMauro.js
@@ -2,6 +2,31 @@ angular.module('starter.controllersMauro', [])
 
 .controller('KontakteCtrl', function($scope, $ionicPopup, $location) {
 
+$scope.openForm = function(){
+
+var form = angular.element( document.querySelector( '#contactForm' ) );
+
+form.removeClass('hidden');
+angular.element( document.querySelector( '#minus' )).removeClass('hidden');
+angular.element( document.querySelector( '#plus' )).removeClass('hidden');
+angular.element( document.querySelector( '#contacts' )).addClass('hidden');
+
+
+};
+$scope.closeForm = function(){
+
+var form = angular.element( document.querySelector( '#contactForm' ) );
+form.addClass('hidden');
+angular.element( document.querySelector( '#minus' )).addClass('hidden');
+angular.element( document.querySelector( '#plus' )).addClass('hidden');
+angular.element( document.querySelector( '#contacts' )).removeClass('hidden');
+
+
+
+};
+
+
+
   $scope.showPrompt = function() {
  var promptPopup = $ionicPopup.prompt({
     title: translate="APPOINTMENTS",
