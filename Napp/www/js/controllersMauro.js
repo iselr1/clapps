51aa0621c822ed780f720eff88d686711fa6c147
tschm2 <emautsc2@gmail.com>
2016-11-12 13:19:40 +0100

    Konakte
    Add Contacts hinzuägfüägt

--- Napp/www/js/controllersMauro.js
+++ Napp/www/js/controllersMauro.js
@@ -1,11 +1,32 @@
 angular.module('starter.controllersMauro', [])
 
-.controller('KontakteCtrl', function($scope, $ionicPopup, $location) {
+.controller('KontakteCtrl', ['$scope', function($scope, $location) {
+$scope.myContacts = [];
+$scope.addContact = {};
+$scope.addContact = function(){
+
+  /*  var contact = [{
+      func:$scope.addContact.func,
+      name:$scope.addContact.nm,
+      street:$scope.addContact.street,
+      location:$scope.addContact.loc,
+      phone:$scope.addContact.phone,
+      mail:$scope.addContact.mail}];
+
+    $scope.myContacts.push(contact);*/
+    $scope.myContacts.push({
+    func:$scope.addContact.func,
+    name:$scope.addContact.nm,
+    street:$scope.addContact.street,
+    location:$scope.addContact.loc,
+    phone:$scope.addContact.phone,
+    mail:$scope.addContact.mail});
+}
+
 
 $scope.openForm = function(){
 
 var form = angular.element( document.querySelector( '#contactForm' ) );
-
 form.removeClass('hidden');
 angular.element( document.querySelector( '#minus' )).removeClass('hidden');
 angular.element( document.querySelector( '#plus' )).removeClass('hidden');
@@ -21,27 +42,8 @@ angular.element( document.querySelector( '#minus' )).addClass('hidden');
 angular.element( document.querySelector( '#plus' )).addClass('hidden');
 angular.element( document.querySelector( '#contacts' )).removeClass('hidden');
 
-
-
 };
-
-
-
-  $scope.showPrompt = function() {
- var promptPopup = $ionicPopup.prompt({
-    title: translate="APPOINTMENTS",
-    template: 'This is prompt popup'
- });
- promptPopup.then(function(res) {
-    if (res) {
-       console.log('Your input is ', res);
-    } else {
-       console.log('Please enter input');
-    }
- });
-
-};
-  })
+  }])
   .controller('InfosCtrl', function($scope, $location) {
 
 
