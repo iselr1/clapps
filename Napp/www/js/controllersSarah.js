22b0dbddd5c797ecd3acf201f0e292cdc65caf89
sarahmele <sarah.mele@students.bfh.ch>
2016-11-23 16:56:50 +0100

    mobiscroll und change language
    jojo

--- Napp/www/js/controllersSarah.js
+++ Napp/www/js/controllersSarah.js
@@ -11,11 +11,39 @@ Funktion: Alle Controller für die Views "Welcome", "TNM-Staging", "Operation"
 angular.module('starter.controllersSarah', [])
 
 /* -- Controller für Welcome View -- */
-.controller('WelcomeCtrl', function($scope, $location, $state) {
+.controller('WelcomeCtrl', function($scope, $location, $state, $translate) {
 
   // Weiterleitung nach TNM Staging
   $scope.goTNM = function() {
     $state.go('tnm');
+  };
+
+  //Choice for language
+  $scope.languageList = [{
+    id: '1',
+    name: 'Deutsch',
+    token: 'de'
+  }, {
+    id: '2',
+    name: 'Français',
+    token: 'fr'
+  }];
+
+  // Set the selectoption to the current language
+  $scope.languageSelected = currentLanguage();
+
+  // Get the current language
+  function currentLanguage() {
+    if (($translate.proposedLanguage() || $translate.use()) == "fr") {
+      return '2';
+    } else {
+      return '1';
+    }
+  };
+
+  // Change language if the selectoption changed
+  $scope.onchangeLanguage = function(key) {
+    $translate.use(key.token);
   };
 
 })
@@ -37,6 +65,19 @@ angular.module('starter.controllersSarah', [])
     $scope.goHome = function() {
     $state.go('home');
   };
+
+
+/*.module('demoApp', ['mobiscroll-datetime'])
+.controller('demoController', ['$scope', function ($scope) {}]);*/
+
+/*  var now = new Date(),
+  max = new Date(now.getFullYear() + 100, now.getMonth(), now.getDate());
+
+  var instance = mobiscroll.date('#demo', {
+    theme: 'mobiscroll',
+    display: 'bottom',
+    max: max
+});*/
 
 })
 ;
