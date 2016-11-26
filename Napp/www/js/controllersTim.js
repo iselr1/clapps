33e2de70ee6b9c2a7da7b3aaaab60affea2910a1
dornt1 <timlucas.dorner@students.bfh.ch>
2016-11-26 15:13:47 +0100

    Timeline controller
    appointments mit daten erstellt

--- Napp/www/js/controllersTim.js
+++ Napp/www/js/controllersTim.js
@@ -4,10 +4,38 @@ angular.module('starter.controllersTim', [])
 .controller('TermineCtrl', function($scope, $state) {
 
 
+      var opDate = new Date(16,03,10);
+
+      var schema = "stadium1";
+
+      var history = [ ];
+
+      if(schema = "stadium1"){
+
+      var appointment = {Date: opDate.setMonth(opDate.getMonth() + 12);  , Description:"Koloskopie Termin 1. Und Labormessung CEA Werts Im Spitalzentrum bei Dr. Viehl"};
+      history[0] = appointment;
+      appointment = {Date: opDate.setMonth(opDate.getMonth() + 24) ,Description:"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed d"};
+      history[1] = appointment;
+      appointment = {Date: opDate.setMonth(opDate.getMonth() + 48) ,Description:"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed d"};
+      history[2] = appointment;
+      appointment = {Date: opDate.setMonth(opDate.getMonth() + 60) ,Description:"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed d"};
+      history[3] = appointment;
+
+      }
+      else if (schema = "stadium2+3"){
+
+
+      }
+      else if (schema = "AlleStadien"){
+
+
+      }
 
       $scope.myAppointments = [];
 
       /*---------------Generating Dummy-Data START----------------*/
+
+
 
       $scope.myAppointments.push({
       state:"DONE APPOINTMENTS",
