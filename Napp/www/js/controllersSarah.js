531283ed3281c8e0f72446938b185484ecc7ed43
iselr1 <reasvenja.iseli@students.bfh.ch>
2016-12-07 23:35:49 +0100

    Schema erstellen funktioniert
    Staging  & Koloskopie funktionieren auch -> somit kann das Schema
    generiert werden

--- Napp/www/js/controllersSarah.js
+++ Napp/www/js/controllersSarah.js
@@ -49,62 +49,78 @@ angular.module('starter.controllersSarah', ['proton.multi-list-picker'])
 })
 
 /* -- Controller für TNM Staging View -- */
-.controller('TNMCtrl', function($scope, $location, $state, schemaService){
+.controller('TNMCtrl', function($scope, $location, $state, schemaService) {
 
-$scope.showSelectValue = function(cancertype) {
-    console.log(cancertype);
-    schemaService.setCancertype(cancertype);
-}
-
-$scope.showSelectTValue = function(tstage) {
-    console.log(tstage);
-    //schemaService.setStagingT(tstage);
-}
-
-    // Testing of Schema generation
-  /*
-    schemaService.setStagingT('1');
-    schemaService.setStagingN('0');
-    schemaService.setStagingM('0');
-  */
+  $scope.showSelectValue = function(cancertype) {
+      console.log(cancertype);
+      schemaService.setCancertype(cancertype);
+    }
+    // Initialize an array for the stages
+  $scope.value = {};
 
 
   // Weiterleitung nach Operationen
   $scope.goOP = function() {
-    //console.log($scope.cancertype);
+    // Set the values of the diffrent stages
+    $scope.$watch('value.tstage', function() {
+      console.log($scope.value.tstage);
+      schemaService.setStagingT($scope.value.tstage);
+    })
+    $scope.$watch('value.nstage', function() {
+      console.log($scope.value.nstage);
+      schemaService.setStagingN($scope.value.nstage);
+    })
+    $scope.$watch('value.mstage', function() {
+      console.log($scope.value.mstage);
+      schemaService.setStagingM($scope.value.mstage);
+    })
     $state.go('op');
   };
 
 })
 
 /* -- Controller für Operationen View -- */
-.controller('OPCtrl', function($scope, $location, $state, ionicDatePicker, $filter, schemaService) {
+.controller('OPCtrl', function($scope, $location, $state, ionicDatePicker, $filter, schemaService, jsonService) {
+
+  var jsonData = jsonService.getJson();
+  var yes = jsonData.YES;
+  var no = jsonData.NO;
+
+  // Functions for the coloskopie toggle
+  $scope.complete = false;
+  $scope.setComplete = function(boolean) {
+    $scope.complete = boolean;
+    console.log($scope.complete);
+  }
+  $scope.setComplete.text = $scope.complete ? yes : no;
+  $scope.update = function() {
+    $scope.setComplete.text = $scope.complete ? yes : no;
+  }
 
 
-  /*schemaService.setColonoscopyComplete(true);
-  */
 
-    // Weiterleitung nach Home Screen
+  $scope.buttonOPText = 'dd.mm.yyyy';
+
+  var ipObj1 = {
+    callback: function(val) { //Mandatory
+      console.log('Return value from the datepicker popup is : ' + val, new Date(val));
+      schemaService.setOpDate(new Date(val));
+      var dateAsString = $filter('date')(val, "dd.MM.yyyy");
+      console.log('Return value from the datepicker popup is formatted : ' + dateAsString);
+      $scope.buttonOPText = dateAsString;
+      //  document.getElementById('opdate').value = dateAsString;
+    }
+  };
+
+  $scope.openDatePicker = function() {
+    ionicDatePicker.openDatePicker(ipObj1);
+  };
+
+  // Go to the Homescreen & set the coloskopie value and generate the Schema
   $scope.goHome = function() {
+    schemaService.setColonoscopyComplete($scope.complete);
+    console.log($scope.complete);
     schemaService.genSchema();
     $state.go('home');
   };
-
- $scope.buttonOPText = 'dd.mm.yyyy';
-
-  var ipObj1 = {
-  callback: function (val) {  //Mandatory
-    console.log('Return value from the datepicker popup is : ' + val, new Date(val));
-    schemaService.setOpDate(new Date(val));
-    var dateAsString = $filter('date')(val, "dd.MM.yyyy");
-    console.log('Return value from the datepicker popup is formatted : ' + dateAsString);
-    $scope.buttonOPText = dateAsString;
-  //  document.getElementById('opdate').value = dateAsString;
-  }};
-
-$scope.openDatePicker = function(){
-  ionicDatePicker.openDatePicker(ipObj1);
-};
-
-})
-;
+});
